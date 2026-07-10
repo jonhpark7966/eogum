d35043cb99dd41df90b1270e7e1d9c07ac3228fb
Jong Hyun Park <jonhpark7966@gmail.com>
2026-07-10 21:46:14 +0900

    Make riverside review project public

--- apps/api/src/eogum/public_access.py
+++ apps/api/src/eogum/public_access.py
@@ -4,6 +4,7 @@ from eogum.config import settings
 _BUILTIN_PUBLIC_PROJECT_IDS = {
     "3d2587aa-f65a-4746-a454-30bba7611ddc",
     "b094cf1c-bf9b-49f1-8a45-c646e3734692",
+    "296ec362-a5eb-405d-a839-3f65509a3ace",
 }
 
 
